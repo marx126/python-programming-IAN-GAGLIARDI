975c9a3798490a413099baf9f0c6ed8afd274fe8
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-10 17:28:29 +0200

    Exercise answer

--- Exercises/00-Count-with-Python-exercises/1- Pythagorean theorem.py
+++ Exercises/00-Count-with-Python-exercises/1- Pythagorean theorem.py
@@ -1,1 +1,19 @@
 # Pythagorean theorem
+import math
+
+#Solution 1 to question a)
+a = 3
+b = 4
+hypotenuse = math.sqrt(a**2 + b**2)
+
+print(f"The hypotenuse is {int(hypotenuse)} lenght units")
+
+#Solution 2 to question a)
+hypotenuse2 = math.hypot(a, b)
+
+#Solution question b)
+c = 7.0
+d = 5.0
+cathetus = math.sqrt(c**2 - d**2)
+
+print(f"The other cathetus is {round(cathetus, 1)} length units")
