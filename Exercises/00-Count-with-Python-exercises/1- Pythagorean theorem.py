95d84434f20f3407e1a2bded12913013a7aa6e7e
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-10 19:35:51 +0200

    excercise update

--- Exercises/00-Count-with-Python-exercises/1- Pythagorean theorem.py
+++ Exercises/00-Count-with-Python-exercises/1- Pythagorean theorem.py
@@ -6,7 +6,7 @@ a = 3
 b = 4
 hypotenuse = math.sqrt(a**2 + b**2)
 
-print(f"The hypotenuse is {int(hypotenuse)} lenght units")
+print(f"The hypotenuse is {int(hypotenuse)} lenght units.")
 
 #Solution 2 to question a)
 hypotenuse2 = math.hypot(a, b)
@@ -16,4 +16,4 @@ c = 7.0
 d = 5.0
 cathetus = math.sqrt(c**2 - d**2)
 
-print(f"The other cathetus is {round(cathetus, 1)} length units")
+print(f"The other cathetus is {round(cathetus, 1)} length units.")
---
+++ Exercises/00-Count-with-Python-exercises/2- Classification accuracy.py
@@ -0,0 +1,5 @@
+#Classification accuracy
+accuracy = 300 / 365
+accuracy_percentage = accuracy * 100
+
+print(f"Machine learning algorithm has an accuracy of {round(accuracy_percentage, 1)}%.")
