84431d4496b7d76e855e5f498db82f1011eea752
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-11 13:16:42 +0200

    Exercise update

--- Exercises/01-if-statement-exercises/1- Check sign.py
+++ Exercises/01-if-statement-exercises/1- Check sign.py
@@ -1,1 +1,10 @@
 # Check sign
+
+number = (float(input("Please type any number: "))) # Decimals are allowed
+
+if number > 0:
+    print(f"{number} is a positive number!")
+elif number < 0:
+    print(f"{number} is a negative number!")
+else:
+    print(f"{number} is zero!")
