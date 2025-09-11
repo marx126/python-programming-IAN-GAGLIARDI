3be02bbeca3aba4e35658664270bd85f04131b16
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-11 15:37:17 +0200

    Exercise update

---
+++ Exercises/02-while-statement-exercises/1- Count numbers.py
@@ -0,0 +1,6 @@
+# Count numbers
+
+i = -10
+while i <= 10:
+    print(i, end=" ")
+    i += 1
---
+++ Exercises/02-while-statement-exercises/2- Arithmetic sum.py
@@ -0,0 +1,17 @@
+# Arithmetic sum
+
+count_1 = 1
+total_1 = 0
+while count_1 <= 100:
+    total_1 += count_1
+    count_1 += 1
+
+print(f"Answer a) {total_1}")
+
+count_2 = 1
+total_2 = 0
+while count_2 <= 100:
+    total_2 += count_2
+    count_2 += 2
+
+print(f"Answer b) {total_2}")
