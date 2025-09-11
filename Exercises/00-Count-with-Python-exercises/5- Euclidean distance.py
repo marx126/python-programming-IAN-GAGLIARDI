9442655e9a6717522b5cd4dd50ca92b218ed47c8
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-11 12:42:13 +0200

    Exercise update

---
+++ Exercises/00-Count-with-Python-exercises/5- Euclidean distance.py
@@ -0,0 +1,9 @@
+# Euclidean distance
+import math
+
+x1, y1 = 3, 5
+x2, y2 = -2, 4
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print(f"The distance is around {round(distance, 1)} length units.")
