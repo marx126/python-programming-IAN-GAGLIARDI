e3a4164c0468da3ace9cec025c35b9b40418f0d6
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-13 11:38:51 +0200

    Exercise update

--- Exercises/04-list-exercises/2- Food menu.py
+++ Exercises/04-list-exercises/2- Food menu.py
@@ -1,1 +1,12 @@
 # Food menu
+foods = ["vegetarisk lasagne", "spaghetti", "fisk", "grönsakssoppa", "pannkakor"]
+weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+print("Bambameny")
+for i in range(len(foods)):
+    print(f"{weekdays[i]}: {foods[i]}")
+
+# I knew my solution could be improved. I found how to use zip on internet:
+print("\nSecond example")
+for day, food in zip(weekdays, foods):
+    print(f"{day}: {food}")
