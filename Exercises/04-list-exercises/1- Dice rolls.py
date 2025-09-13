0e968b4c0ed07e6ccf30e3d073507b04895a3780
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-13 11:21:41 +0200

    Exercise update

--- Exercises/04-list-exercises/1- Dice rolls.py
+++ Exercises/04-list-exercises/1- Dice rolls.py
@@ -1,1 +1,18 @@
 # Dice rolls
+import random
+
+my_list = []
+
+# Using append:
+for i in range(10):
+    my_list.append(random.randint(1,6))
+
+print(f"Ascending order: {sorted(my_list)}\n")
+print(f"Descending order: {sorted(my_list, reverse=True)}\n")
+print(f"Maximum. {max(my_list)}\n")
+print(f"Minimum: {min(my_list)}")
+
+my_list = my_list.sort
+
+# Using list comprehension:
+my_list2 = [random.randint(1, 6) for i in range(10)]
