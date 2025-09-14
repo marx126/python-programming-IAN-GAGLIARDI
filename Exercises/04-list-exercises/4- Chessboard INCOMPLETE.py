8ec35f4c74bed1630322037b889fae51e4cbfcfa
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-14 12:53:17 +0200

    Exercise update

--- Exercises/04-list-exercises/4- Chessboard INCOMPLETE.py
+++ Exercises/04-list-exercises/4- Chessboard INCOMPLETE.py
@@ -1,2 +1,16 @@
 # Chessboard
+letters = "ABCDEFGH"
 
+first_row = [letter+"1" for letter in letters]
+print(f"{first_row}\n")
+
+chessboard = []
+print("Chessboard:")
+for i in range(1, 9):               # Could have used list comprehension but im still not good at it
+    row = []
+    for letter in letters:
+        row.append(f"{letter}{i}")
+    chessboard.append(row)
+
+for row in chessboard:
+    print(row)
