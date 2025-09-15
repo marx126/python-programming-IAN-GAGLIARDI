4dceac934cce2d25783c690eee8aaf28cc926af5
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-15 14:19:40 +0200

    Exercise update

--- Exercises/06-functions-exericses/2- Euclidean distance.py
+++ Exercises/06-functions-exericses/2- Euclidean distance.py
@@ -1,1 +1,16 @@
 # Euclidean distance
+import math
+
+def euclidean(a, b):
+    return round(math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2), 1)
+
+p1 = tuple(map(int, input("Enter first point (x, y): ").split(",")))
+p2 = tuple(map(int, input("Enter second point (x, y): ").split(",")))
+print(euclidean(p1, p2))
+
+my_points = [(10, 3), (-1, -9), (10, -10), (4, -2), (9, -10)]
+distances = []
+
+for tuple in my_points:
+    distances.append(euclidean((0, 0), tuple))
+print(distances)
