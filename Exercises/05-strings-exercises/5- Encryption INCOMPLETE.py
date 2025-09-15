4c2fb71c58cb6a0383284f4727901ff2385f5df7
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-15 12:54:58 +0200

    Exercise update

--- Exercises/05-strings-exercises/5- Encryption INCOMPLETE.py
+++ Exercises/05-strings-exercises/5- Encryption INCOMPLETE.py
@@ -2,12 +2,26 @@
 import string
 abc = (string.ascii_lowercase)
 abc = abc + "ö" + "ä" + "å"
+
+choice = int(input("press 1 to encryption or 2 to decryption: "))
 word = input("Please enter a word: ")
 
-encripted_word = ""
+encrypted_word = ""
+decrypted_word = "" 
 for letter in word:
     if letter in abc:
         position = abc.index(letter)
-        new_position = position + 1
-        encripted_word += abc[new_position]
-print(encripted_word)
+        if choice == 1:
+            if position == len(abc) -1:
+                new_position = 0
+            else:
+                new_position = position + 1
+            encrypted_word += abc[new_position]
+        elif choice == 2:
+            if position == 0:
+                new_position = len(abc) - 1
+            else:
+                new_position = position - 1
+            decrypted_word += abc[new_position]
+
+# Probably not the cleanest code but it works
