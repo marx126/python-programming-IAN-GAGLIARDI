f3bfe94aaec331d8faf742383532c3cdea5ff6ad
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-15 09:17:10 +0200

    Exercise update

--- Exercises/05-strings-exercises/5- Encryption INCOMPLETE.py
+++ Exercises/05-strings-exercises/5- Encryption INCOMPLETE.py
@@ -1,1 +1,11 @@
 # Encryption
+import string
+abc = list(string.ascii_lowercase)
+abc.extend(["ö", "ä", "å"])
+
+word = input("Please enter a word: ")
+lower_word = word.lower
+encripted_word = ""
+
+for i in word:
+    if i in abc:
