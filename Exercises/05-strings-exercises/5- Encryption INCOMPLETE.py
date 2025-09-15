4c6d444ebdd5c5484721f35b5e1b20c5cc2c1494
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-15 12:39:49 +0200

    Exercise update

--- Exercises/05-strings-exercises/5- Encryption INCOMPLETE.py
+++ Exercises/05-strings-exercises/5- Encryption INCOMPLETE.py
@@ -3,10 +3,11 @@ import string
 abc = (string.ascii_lowercase)
 abc = abc + "ö" + "ä" + "å"
 word = input("Please enter a word: ")
-lower_word = word.lower
-encripted_word = ""
 
+encripted_word = ""
 for letter in word:
     if letter in abc:
         position = abc.index(letter)
-        print(position)
+        new_position = position + 1
+        encripted_word += abc[new_position]
+print(encripted_word)
