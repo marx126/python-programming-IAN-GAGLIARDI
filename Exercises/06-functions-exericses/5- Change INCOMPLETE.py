0c6c6e60038b4abb761bfe62797f64a61c05b1a7
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-16 13:13:15 +0200

    Exercise update

---
+++ Exercises/06-functions-exericses/5- Change INCOMPLETE.py
@@ -0,0 +1,9 @@
+# Change
+
+amount = int(input("Please enter an amount of money: "))
+
+def change(money: int):
+    values = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
+    names = ["1000-lapp", "500-lapp", "200-lapp", "100-lapp",
+             "50-lapp", "20-lapp", "10-krona", "5-krona", "2-krona", "1-krona"]
+    
