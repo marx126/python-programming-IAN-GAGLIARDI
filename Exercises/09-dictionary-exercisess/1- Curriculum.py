ef711478248c95ca46d4e43e517fe9b509a581af
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-17 14:24:55 +0200

    Exercise update

--- Exercises/09-dictionary-exercisess/1- Curriculum.py
+++ Exercises/09-dictionary-exercisess/1- Curriculum.py
@@ -1,1 +1,15 @@
 # Curriculum
+courses = {"Introduktionskurs till AI: ":5,
+           "Programmering med Python: ":40,
+           "Databehandling: ":25,
+           "Linjär algebra: ":20,
+           "Statistiska metoder: ":30,
+           "Maskininlärning: ":45,
+           "Databaser: ":25,
+           "Data Engineering och agila metoder: ":45,
+           "LIA 1: ":40,
+           "Djup Maskininlärning: ":40,
+           "LIA 2: ":70,
+           "Examensarbete: ":15}
+
+print(sum(courses.values()))
