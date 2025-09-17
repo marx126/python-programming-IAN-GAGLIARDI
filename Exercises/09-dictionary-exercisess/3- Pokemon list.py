9ebe05cec5636e53e66ecabd9bd83c8899ab3da7
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-17 16:13:59 +0200

    Exercise update

--- Exercises/09-dictionary-exercisess/3- Pokemon list.py
+++ Exercises/09-dictionary-exercisess/3- Pokemon list.py
@@ -1,1 +1,7 @@
 # Pokemon list
+file_path = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Exercises/09-dictionary-exercisess/pokemon_list.txt"
+with open(file_path, "r") as f:
+    pokemon_list = f.readlines()
+
+for i in pokemon_list:
+    print(i.strip())
