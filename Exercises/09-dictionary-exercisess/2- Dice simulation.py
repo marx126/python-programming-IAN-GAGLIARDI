0f77df5b4195206355b938016733b52bc2cd983e
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-17 14:48:10 +0200

    Exercise update

--- Exercises/09-dictionary-exercisess/2- Dice simulation.py
+++ Exercises/09-dictionary-exercisess/2- Dice simulation.py
@@ -1,1 +1,11 @@
 # Dice simulation
+import random
+dice_rolls = [random.randint(1,6) for i in range(1_000_000)]
+
+total_rolls = {i: 0 for i in range(1,7)}
+
+for roll in dice_rolls:
+    total_rolls[roll] += 1
+
+for number, count in total_rolls.items():
+    print(f"{number}: {count}")
