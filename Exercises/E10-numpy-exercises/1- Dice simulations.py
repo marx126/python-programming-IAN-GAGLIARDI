a7177120fda5cb413530686403db03c4bebfc679
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-19 00:41:45 +0200

    Exercise and Labb2 important update

--- Exercises/E10-numpy-exercises/1- Dice simulations.py
+++ Exercises/E10-numpy-exercises/1- Dice simulations.py
@@ -1,1 +1,11 @@
 # Dice simulations
+import numpy as np
+import random
+
+def dice_mean(): # Function to call the theoretical mean of a dice
+    dice_faces = np.arange(1,7)
+    return dice_faces.mean()
+
+print(dice_mean())
+many_dices = np.random.randint(1,6,10)
+print(many_dices)
--- Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/Klassificera Pokémons med Nearest Neig.py
+++ Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/Klassificera Pokémons med Nearest Neig.py
@@ -3,32 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 file_path = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/datapoints.txt"
 
-pikachu_x = []
-pikachu_y = []
-pichu_x = []
-pichu_y = []
+# Read file and create a numpy array
+data = np.loadtxt(file_path, delimiter=",", skiprows=1)
 
-# Read the file and append cleaned data to above pikachu and pichu data lists
-with open(file_path, "r") as f: # Will change to numpy arrays in the future
-    next(f)
-    for line in f:
-        parts = line.strip().split(",")
-        width = float(parts[0])
-        height = float(parts[1])
-        p_type = int(parts[2])
+# Separate width, height and pokemon type
+x = data[:, 0]
+y = data[:, 1]
+p_type = data[:, 2]
 
-        if p_type == 1:
-            pikachu_x.append(width)
-            pikachu_y.append(height)
-        else:
-            pichu_x.append(width)
-            pichu_y.append(height)
+# Boolean array indexing to order x and y by type of pokemon
+pikachu_x = x[p_type == 1]
+pikachu_y = y[p_type == 1]
+pichu_x = x[p_type == 0]
+pichu_y = y[p_type == 0]
 
-plt.scatter(pikachu_x, pikachu_y, color="yellow", label="Pikachu")
-
-plt.scatter(pichu_x, pichu_y, color="blue", label="Pichu")
-
-plt.xlabel("Width (cm)")
-plt.ylabel("Height (cm)")
+# Plot the data in scatter graph
+plt.scatter(pikachu_x,pikachu_y, color="yellow", label="Pikachu")
+plt.scatter(pichu_x,pichu_y, color="blue", label="Pichu")
 plt.legend()
 plt.show()
