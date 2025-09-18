6cae866a0996938ac09beec83ee34295cb481b49
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-18 17:02:22 +0200

    Exercise update

--- Exercises/09-dictionary-exercisess/3- Pokemon list.py
+++ Exercises/09-dictionary-exercisess/3- Pokemon list.py
@@ -1,7 +1,14 @@
 # Pokemon list
 file_path = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Exercises/09-dictionary-exercisess/pokemon_list.txt"
-with open(file_path, "r") as f:
-    pokemon_list = f.readlines()
+pokedex = {}
 
-for i in pokemon_list:
-    print(i.strip())
+with open(file_path, "r") as f:
+    for line in f:
+        parts = line.strip().split()
+        index = parts[0]
+        name = parts[1]
+        pokemon_type = " ".join(parts[2:])
+        pokedex[name] = f"{pokemon_type}, {index}"
+
+print(pokedex["Gengar"])
+print(pokedex["Pikachu"])
