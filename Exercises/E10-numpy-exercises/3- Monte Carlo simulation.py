b9395f6088f65d2b5c2ba11cb05e95819bc5e102
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-19 14:35:18 +0200

    Exercise update

--- Exercises/E10-numpy-exercises/3- Monte Carlo simulation.py
+++ Exercises/E10-numpy-exercises/3- Monte Carlo simulation.py
@@ -1,1 +1,23 @@
 # Monte Carlo simulation
+import numpy as np
+import matplotlib.pyplot as plt
+points = np.random.uniform(-1, 1, (500000, 2))
+points0 = np.random.uniform(0, 0, (500000, 2))
+
+distances = np.linalg.norm(points, axis=1)
+print(distances)
+
+inside = distances < 1
+outside = distances >= 1
+
+plt.scatter(points[inside, 0], points[inside, 1])
+plt.scatter(points[outside, 0], points[outside, 1])
+plt.axis("equal")
+plt.legend()
+plt.title("Simulation of 500000 points")
+plt.show()
+
+total_inside = np.sum(inside)
+total_outside = np.sum(outside)
+fraction = total_inside / (total_inside + total_outside)
+print(fraction)
