9208af697ff7ed02f17466ddfe14a59865b26edc
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-19 13:36:53 +0200

    Exercise update

--- Exercises/E10-numpy-exercises/2- Several dices INCOMPLETE.py
+++ Exercises/E10-numpy-exercises/2- Several dices INCOMPLETE.py
@@ -1,24 +1,40 @@
 # Several dices
 import numpy as np
 import matplotlib.pyplot as plt
+def sample_space():
+    dice = np.arange(1, 7)
+    x, y = np.meshgrid(dice, dice)
+    sums = x + y
+    return sums
 
-dice = np.arange(1, 7)
+def unique_and_counts(sums):
+    unique, counts = np.unique(sums, return_counts=True)
+    return unique, counts
 
-x, y = np.meshgrid(dice, dice)
-sums = x + y
-print(f"Sample space\n{sums}")
+def frequency(counts):
+    total = counts.sum()
+    frequencies = np.round(counts / total, 3)
+    return frequencies
 
+def plot_two_dices(unique, frequencies):    
+    plt.bar(unique, frequencies)
+    plt.xlabel("Unique values")
+    plt.ylabel("Frequency")
+    plt.title("Sum of two dices")
+    return plt.show()
 
-unique, counts = np.unique(sums, return_counts=True)
-print(f"\nUnique values\n{unique}")
-print(f"\nCounts\n{counts}")
+def main():
+    sums = sample_space()
+    print(f"Sample space\n{sums}")
 
-total = counts.sum()
-frequencies = np.round(counts / total, 3)
-print(f"\nFrequencies\n{frequencies}")
+    unique, counts = unique_and_counts(sums)
+    print(f"\nUnique values\n{unique}")
+    print(f"\nCounts\n{counts}")
 
-plt.bar(unique, frequencies)
-plt.xlabel("Unique values")
-plt.ylabel("Frequency")
-plt.title("Sum of two dices")
-plt.show()
+    frequencies = frequency(counts)
+    print(f"\nFrequencies\n{frequencies}")
+
+    plot_two_dices(unique, frequencies)
+
+if __name__ == "__main__":
+    main()
