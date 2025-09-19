cba504f37b5906aefac0a09dcef1a6f1de1f8f7c
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-19 02:15:40 +0200

    Labb2 update

--- Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/Klassificera Pokémons med Nearest Neig.py
+++ Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/Klassificera Pokémons med Nearest Neig.py
@@ -2,9 +2,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 data_points = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/datapoints.txt"
-test_points = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/testpoints.txt"
+#test_points = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/testpoints.txt"
 
-def load_data(data_path) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+def load_data(data_path) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     # Read file and create a numpy array
     data = np.loadtxt(data_path, delimiter=",", skiprows=1)
     # Separate width, height and pokemon type
@@ -18,7 +18,7 @@ def load_data(data_path) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     pichu_y = y[p_type == 0]
     return pikachu_x, pikachu_y, pichu_x, pichu_y
 
-def plot_data_points(pikachu_x, pikachu_y, pichu_x, pichu_y) -> plt.show:
+def plot_data_points(pikachu_x, pikachu_y, pichu_x, pichu_y):
     plt.scatter(pikachu_x,pikachu_y, color="yellow", label="Pikachu")
     plt.scatter(pichu_x,pichu_y, color="blue", label="Pichu")
     plt.legend()
@@ -28,4 +28,5 @@ def main():
     pikachu_x, pikachu_y, pichu_x, pichu_y = load_data(data_points)
     plot_data_points(pikachu_x, pikachu_y, pichu_x, pichu_y)
 
-main()
+if __name__ == "__main__":
+    main()
