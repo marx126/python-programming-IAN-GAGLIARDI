fa5b0cbb3def0840c6e53693a307deb8aadcb60f
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-20 13:22:14 +0200

    Exercise update

--- Exercises/E11-OOP-basic-exercises/1- Unit conversion.py
+++ Exercises/E11-OOP-basic-exercises/1- Unit conversion.py
@@ -4,11 +4,24 @@ class UnitUS:
     def __init__(self, value):
         self.value = value
 
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, (int, float)):
+            raise TypeError("Value must be an int or float")
+        
+        if new_value <= 0:
+            raise ValueError("Value must be a positive number")
+        self._value = new_value
+
     def inch_to_cm(self):
         return self.value * 2.54
 
     def foot_to_meters(self):
-        return self.value / 3.281
+        return round(self.value / 3.281, 3)
 
     def pound_to_kg(self):
         return self.value / 2.205
@@ -16,8 +29,8 @@ class UnitUS:
     def __repr__(self):
         return f"UnitUS(value={self.value})"
 
-test = UnitUS(5)
-print(test.inch_to_cm())
-print(test.foot_to_meters())
-print(test.pound_to_kg())
-print(test)
+units = UnitUS(5)
+print(f"5 feet = {units.foot_to_meters()} m")
+print(f"5 inch = {units.inch_to_cm()} cm")
+print(f"5 pounds = {units.pound_to_kg():.2f} kg")
+print(units)
