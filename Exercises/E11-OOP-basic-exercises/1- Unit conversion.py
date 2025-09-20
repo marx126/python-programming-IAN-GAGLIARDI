6fc9a7b680145ae3a6909dcc56801d64de3167b8
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-20 12:51:12 +0200

    Exercise update

--- Exercises/E11-OOP-basic-exercises/1- Unit conversion.py
+++ Exercises/E11-OOP-basic-exercises/1- Unit conversion.py
@@ -1,1 +1,23 @@
 # Unit conversion
+class UnitUS:
+    """Class for converting US units to the metric system"""
+    def __init__(self, value):
+        self.value = value
+
+    def inch_to_cm(self):
+        return self.value * 2.54
+
+    def foot_to_meters(self):
+        return self.value / 3.281
+
+    def pound_to_kg(self):
+        return self.value / 2.205
+
+    def __repr__(self):
+        return f"UnitUS(value={self.value})"
+
+test = UnitUS(5)
+print(test.inch_to_cm())
+print(test.foot_to_meters())
+print(test.pound_to_kg())
+print(test)
