b5dfd7b45f9c0cee450eddcf177e4ea1bd53abb7
Ian Gagliardi <iangagliardi@gmail.com>
2025-09-21 23:20:47 +0200

    Labb2 update

--- Exercises/E11-OOP-basic-exercises/3- Student and Teacher.py
+++ Exercises/E11-OOP-basic-exercises/3- Student and Teacher.py
@@ -1,1 +1,53 @@
 # Student and Teacher
+class Person:
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, (str)):
+            raise TypeError("Value must be a string")
+        self._name = new_name
+
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, new_age):
+        if not isinstance(new_age, (int, float)):
+            raise TypeError("Value must be an intiger or float")
+        if new_age <= 0:
+            raise ValueError("Value must be a positive number")
+        self._age = new_age
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, new_email):
+        if not isinstance(new_email, (str)):
+            raise TypeError("Value must be a string")
+        if not validators.email(new_email):
+            raise TypeError(f"{new_email} is not a valid email, format must be xxxx@yyyy.zzz")
+        self._email = new_email
+
+    def __repr__(self):
+        return f"Hi, my name is {self._name}, I am {self._age} years old, my email address is {self._email}"
+    
+class Student(Person):
+    def __init__(self, name, age, email):
+        super().__init__(name, age, email)
+        pass
+
+class Teacher(Person):
+    def __init__(self, name, age, email):
+        super().__init__(name, age, email)
+        pass
--- Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/Klassificera Pokémons med Nearest Neig.py
+++ Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/Klassificera Pokémons med Nearest Neig.py
@@ -1,6 +1,7 @@
 # Klassificera Pokémons med Nearest Neighbour algoritmen
 import matplotlib.pyplot as plt
 import numpy as np
+
 data_points = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/datapoints.txt"
 test_points = "/Users/ianpablogagliardibianchini/Projects/python-programming-IAN-GAGLIARDI/Labs/Laboration 2 - Icke-Generaliserande Maskinintelligens/testpoints.txt"
 
@@ -24,9 +25,24 @@ def plot_data_points(pikachu_x, pikachu_y, pichu_x, pichu_y):
     plt.legend()
     return plt.show()
 
+def load_test_points(test_points):
+    with open(test_points, "r") as f:
+        next(f)
+        points = []
+        for line in f:
+            if "(" in line:
+                data = line.split("(")[1].split(")")[0]
+                x, y = map(float, data.split(","))
+                points.append([x, y])
+    return np.array(points)
+
+def euclidean_distance(p1, p2):
+    pass
+
 def main():
     pikachu_x, pikachu_y, pichu_x, pichu_y = load_data(data_points)
     plot_data_points(pikachu_x, pikachu_y, pichu_x, pichu_y)
+
 
 if __name__ == "__main__":
     main()
